abb93ea17fa0c977e8a6f3cb6c33519eae032af8
Maxrey88 <maximiliano.reyes@aicore.com.ar>
2022-09-16 00:26:50 -0300

    Test post init script add libraries

---
+++ scripts/postScript.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import ora from 'ora';
+import { inquire } from './generator';
+
+const spinner = ora('Optional libraries setup');
+
+new Promise((resolve) => {
+  spinner.start();
+  inquire(resolve);
+})
+  .then(() => {
+    spinner.succeed();
+  })
+  .catch((error) => {
+    spinner.fail(error);
+    throw new Error(
+      'Something went wrong during the post init script execution',
+    );
+  });
