cf51c98e6ed839deea69dba6ebf619be22c3ef59
Maxrey88 <maximiliano.reyes@aicore.com.ar>
2022-09-16 00:10:48 -0300

    Fixes some script

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
 #!/usr/bin/env node
-console.info('The Base Strcture Template initialized with success!!!')
-/* import ora from 'ora';
-import { inquire } from './generator';
 
-const spinner = ora('Optional libraries setup');
-
-new Promise((resolve) => {
-  spinner.start();
-  inquire(resolve);
-})
-  .then(() => {
-    spinner.succeed();
-  })
-  .catch((error) => {
-    spinner.fail(error);
-    throw new Error(
-      'Something went wrong during the post init script execution',
-    );
-  }); */
+console.log("This is post init script");
