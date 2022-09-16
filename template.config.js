fd0b9679305099e2a0fb17588bf799ea7b8fa3a2
Maxrey88 <maximiliano.reyes@aicore.com.ar>
2022-09-15 21:24:38 -0300

    Fix template config

--- template.config.js
+++ template.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     placeholderName: 'ReactNativeTemplate',
     templateDir: './template',
-    postInitScript: './scripts/postInitScript.js',
+    postInitScript: './scripts/postInitScript',
   };
