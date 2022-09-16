17f099f2cf5888e65c134082b4e8528ddd583336
Maxrey88 <maximiliano.reyes@aicore.com.ar>
2022-09-16 20:23:28 -0300

    Add auth0 integrations

--- template/App.js
+++ template/App.js
@@ -6,6 +6,7 @@
  * @flow strict-local
  */
 
+import Auth0 from 'react-native-auth0';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
@@ -13,10 +14,23 @@ import {
   SafeAreaView,
   StatusBar,
 } from 'react-native';
+import { AUTH0_DOMAIN, AUTH0_CLIENT_ID } from '@env'
 import HomeScreen from './src/screens/HomeScreen';
+
+const auth0 = new Auth0({ domain: AUTH0_DOMAIN, clientId: AUTH0_CLIENT_ID });
 
 const App = () => {
   const Stack = createNativeStackNavigator()
+
+  const login = async (isSignUp) => {
+    try {
+      const credentials = await auth0.webAuth.authorize({scope: 'openid profile email'})
+      console.log(credentials);
+    } catch (error) {
+      console.log('AUTH0 LOGIN --->', error);
+    }
+  }
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <StatusBar barStyle={'dark-content'} />
--- template/ios/BaseStructure/AppDelegate.mm
+++ template/ios/BaseStructure/AppDelegate.mm
@@ -27,7 +27,15 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 @end
 #endif
 
+#import <React/RCTLinkingManager.h>
+
 @implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options
+{
+  return [RCTLinkingManager application:app openURL:url options:options];
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
